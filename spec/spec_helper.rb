ab6d676f5934da22801c9e12cc4a746ed0e0a241
Laertis Pappas <laertis.pappas@gmail.com>
2015-10-03 13:20:42 +0300

    Added rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,24 +14,24 @@ ActiveRecord::Base.establish_connection(
 DatabaseCleaner.strategy = :truncation
 
 
-Rspec.configure do |config|
-#  config.before(:suite) do
-#    DatabaseCleaner.strategy = :transaction
-#    DatabaseCleaner.clean_with(:truncation)
-#  end
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-#  config.around(:each) do |example|
-#    DatabaseCleaner.cleaning do
-#      example.run
-#    end
-#  end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 
-#  config.before(:each) do
-#    DatabaseCleaner.start
-#  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
 
-#  config.after(:each) do
-#    DatabaseCleaner.clean
-#  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
 end
